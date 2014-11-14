b14511ce74a716f1a7976c0083a19551c0a57a09
ruijie <liruijie0603@126.com>
2014-11-14 19:11:10 +0800

    添加Exception

---
+++ src/main/java/com/reyun/api/exception/AppidNotValidException.java
@@ -0,0 +1,9 @@
+package com.reyun.api.exception;
+
+public class AppidNotValidException extends ReyunException {
+	private static final long serialVersionUID = 6773924799307239681L;
+
+	public AppidNotValidException() {
+		super();
+	}
+}
---
+++ src/main/java/com/reyun/api/exception/ParamRequiredException.java
@@ -0,0 +1,9 @@
+package com.reyun.api.exception;
+
+public class ParamRequiredException extends ReyunException {
+	private static final long serialVersionUID = 1739996258771929045L;
+
+	public ParamRequiredException(String str) {
+		super(str);
+	}
+}
