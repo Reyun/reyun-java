453b1c4607d2c1f627c67fb4b3e48b4e37ef2d15
ruijie <liruijie0603@126.com>
2014-11-18 11:03:17 +0800

    删除Config

--- src/main/java/com/reyun/api/Config.java
+++
@@ -1,8 +0,0 @@
-package com.reyun.api;
-
-public class Config {
-
-//	public static final String BASE_ENDPOINT = "http://127.0.0.1:80/receive/";
-	public static final String BASE_ENDPOINT = "http://log.reyun.com/receive/";
-	public static final int MAX_MESSAGE_SIZE = 50;
-}
