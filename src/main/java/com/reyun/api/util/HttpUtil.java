9f4453ae7f3b875b9e7cb20a2da7249b2ffe40b5
ruijie <liruijie0603@126.com>
2015-08-17 19:27:50 +0800

    format file

--- src/main/java/com/reyun/api/util/HttpUtil.java
+++ src/main/java/com/reyun/api/util/HttpUtil.java
@@ -35,79 +35,79 @@ import com.reyun.api.model.Model;
  */
 public class HttpUtil {
     private static final Logger logger = LoggerFactory.getLogger(HttpUtil.class);
-    
-	private static final String mEventsEndpoint = "http://log.reyun.com/receive/rest/";
-	
-	private static HttpClientBuilder httpBulder;
-	private static PoolingHttpClientConnectionManager connectionManager;
-	
-	static {
-	    connectionManager = new PoolingHttpClientConnectionManager();
-	    connectionManager.setMaxTotal(500);
+
+    private static final String mEventsEndpoint = "http://log.reyun.com/receive/rest/";
+
+    private static HttpClientBuilder httpBulder;
+    private static PoolingHttpClientConnectionManager connectionManager;
+
+    static {
+        connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(500);
         connectionManager.setDefaultMaxPerRoute(500);
         httpBulder = HttpClients.custom();
         httpBulder.setConnectionManager(connectionManager);
-        
+
         ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(1);
         scheduledThreadPool.scheduleWithFixedDelay(new Runnable() {
-            
+
             @Override
             public void run() {
                 PoolStats stats = connectionManager.getTotalStats();
                 logger.debug(stats.toString());
             }
-        }, 5, 30,TimeUnit.SECONDS);
-	}
-	
-	@SuppressWarnings("rawtypes")
+        }, 5, 30, TimeUnit.SECONDS);
+    }
+
+    @SuppressWarnings("rawtypes")
     public static final Result post(Model model, int timeout) throws ConnectException, ReyunTimeoutException, IOException {
-		Result result = new Result();
-		
-		try {
-		    CloseableHttpClient client = httpBulder.build();
-	        String url = mEventsEndpoint + model.method();
-	        
-	        HttpPost post = new HttpPost(url);
-	        post.setHeader("Content-Type", "application/json;charset=UTF-8");
-	        
-	        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(timeout).setConnectTimeout(timeout).build();
-	        post.setConfig(requestConfig);
-	        
-	        String request = model.toString();
-	        StringEntity se = new StringEntity(request, Charset.forName("UTF-8"));
-	        post.setEntity(se);
-	        
-	        CloseableHttpResponse response = client.execute(post);
-	        
-	        String responseStr = EntityUtils.toString(response.getEntity());
-	        
-	        JSONObject responseJSON = JSON.parseObject(responseStr);
-	        
-	        JSONObject resultJSON = new JSONObject();
-	        resultJSON.put("url", url);
-	        resultJSON.put("requestStr", request);
-	        resultJSON.put("httpcode", response.getStatusLine().getStatusCode());
-	        resultJSON.put("responseStr", responseStr);
-	        
-	        result.setMessage(resultJSON.toJSONString());
-	        
-	        if (responseJSON.getIntValue("status") == 0) {
-	            result.setStatus(true);
-	        } else {
-	            result.setStatus(false);
-	        }
-	        
-	        response.close();
-		} catch (ConnectTimeoutException | SocketTimeoutException e) {
+        Result result = new Result();
+
+        try {
+            CloseableHttpClient client = httpBulder.build();
+            String url = mEventsEndpoint + model.method();
+
+            HttpPost post = new HttpPost(url);
+            post.setHeader("Content-Type", "application/json;charset=UTF-8");
+
+            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(timeout).setConnectTimeout(timeout).build();
+            post.setConfig(requestConfig);
+
+            String request = model.toString();
+            StringEntity se = new StringEntity(request, Charset.forName("UTF-8"));
+            post.setEntity(se);
+
+            CloseableHttpResponse response = client.execute(post);
+
+            String responseStr = EntityUtils.toString(response.getEntity());
+
+            JSONObject responseJSON = JSON.parseObject(responseStr);
+
+            JSONObject resultJSON = new JSONObject();
+            resultJSON.put("url", url);
+            resultJSON.put("requestStr", request);
+            resultJSON.put("httpcode", response.getStatusLine().getStatusCode());
+            resultJSON.put("responseStr", responseStr);
+
+            result.setMessage(resultJSON.toJSONString());
+
+            if (responseJSON.getIntValue("status") == 0) {
+                result.setStatus(true);
+            } else {
+                result.setStatus(false);
+            }
+
+            response.close();
+        } catch (ConnectTimeoutException | SocketTimeoutException e) {
             throw new ReyunTimeoutException(e);
         } catch (ConnectException e) {
-		    throw e;
-		} catch (IOException e) {
-		    throw e;
+            throw e;
+        } catch (IOException e) {
+            throw e;
         } catch (Exception e) {
             throw e;
         }
-		
-		return result;
-	}
+
+        return result;
+    }
 }
