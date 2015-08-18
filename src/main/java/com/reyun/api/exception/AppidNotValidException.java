4d8ff4119f8db61d52fa4c32e4889b3c75ffdd37
ruijie <liruijie0603@126.com>
2015-08-18 17:06:56 +0800

    AppidNotValidException: add param

--- src/main/java/com/reyun/api/exception/AppidNotValidException.java
+++ src/main/java/com/reyun/api/exception/AppidNotValidException.java
@@ -3,7 +3,7 @@ package com.reyun.api.exception;
 public class AppidNotValidException extends Exception {
 	private static final long serialVersionUID = 6773924799307239681L;
 
-	public AppidNotValidException() {
-		super();
+	public AppidNotValidException(String str) {
+		super(str);
 	}
 }
